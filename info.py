06c3f8669d041b3e32186cd9b32c7db7f5faa3f6
Peterparker <89297664+peterparker088@users.noreply.github.com>
2022-05-14 15:04:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,17 @@ P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE","<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE","""<b>{title}</b>
+
+<b>🧬 Genre:</b> {genres}
+<b>🌟 Rating:</b> {rating}
+<b>⏱️ Duration:</b> {runtime}
+<b>📆 Release:</b> {year}
+<b>🎙 Language:</b> {languages}
+
+<b>💭 Requested By:</b> <tg-spoiler>||{message.from_user.mention}||</tg-spoiler>
+
+<b>🔰 Powered By:</b> <b>{message.chat.title}</b>""")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
